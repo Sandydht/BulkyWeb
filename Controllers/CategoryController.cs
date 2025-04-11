38f6b8c37c30f4c17fe51dc81da03c392f31efeb
sandy <sandydwi75@gmail.com>
2025-04-12 00:46:56 +0700

    feat: print category list

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers;
 
 public class CategoryController : Controller
 {
+  private readonly ApplicationDbContext _dbContext;
+
+  public CategoryController(ApplicationDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+
   public IActionResult Index()
   {
-    return View();
+    List<Category> objectCategoryList = _dbContext.Categories.ToList();
+    return View(objectCategoryList);
   }
 }
